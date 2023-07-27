b4dc8502b049745f3f5cc3701ed2b08cdb11d121
Abhay Malvi <doepicshit@pop-os.localdomain>
2023-07-27 16:39:53 +0530

    New Feature added

--- src/Chat/ChatMessage.js
+++ src/Chat/ChatMessage.js
@@ -6,7 +6,7 @@ const ChatMessage = ({ message }) => {
   // current user
   const userInfo = useContext(UserContext);
 
-  const { user, type, chatMessage } = message;
+  const { user, type, chatMessage, fileUrl, fileName } = message;
   const selfClass = userInfo.username === user ? "message-chat-self" : "";
 
   // render bot messages
@@ -18,13 +18,47 @@ const ChatMessage = ({ message }) => {
     );
   }
 
+  const renderFileMsg = () => {
+    switch(type)
+    {
+      case "IMAGE":
+        return(<div className={`message message-chat message-media ${selfClass}`}>
+            <img
+                className="chat-image"
+                src={fileUrl}
+            />
+        </div>);
+      case "VIDEO":
+        return (
+          <div
+              className={`message message-chat message-media ${selfClass}`}>
+              <video className="chat-video" controls>
+                  <source src={fileUrl} type="video/mp4" />
+                  Your browser does not support the video tag.
+              </video>
+          </div>
+      );
+
+      case "FILE":
+        return (
+            <div
+                className={`message message-chat message-media ${selfClass}`}>
+                <a className="chat-file" href={fileUrl} target="_blank">
+                    <i className="fa fa-file fa-2x mr-2"></i>
+                    <span>{fileName}</span>
+                </a>
+            </div>
+        );
+    }
+  };
+
   return (
     <div className={`message message-chat ${selfClass}`}>
       <p>
         {selfClass === "" ? <small> {user} </small> : <small> You </small>}
         <small>{moment().format("h:m a")}</small>
       </p>
-      <p>{chatMessage}</p>
+      {type === 'TEXT' ? <p>{chatMessage}</p> : renderFileMsg()}
     </div>
   );
 };
